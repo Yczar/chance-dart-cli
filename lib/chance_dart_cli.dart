f7452df85e9bb53aa9bf3f44cbab86337537b4dc
Yczar <babalolagbogo@gmail.com>
2022-11-08 15:05:08 +0100

    chore: created chance_dart_cli.dart

---
+++ lib/chance_dart_cli.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+/// chance_dart_cli, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate chance_dart_cli
+/// dart pub global activate chance_dart_cli
+///
+/// # see usage
+/// chance_dart_cli --help
+/// ```
+library chance_dart_cli;
